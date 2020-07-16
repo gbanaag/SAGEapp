42902c5c140ef98913ceafc2332e7f85389a2a07
gabbybanaag <gb2646@columbia.edu>
2020-07-16 10:00:31 -0700

    first commit

---
+++ SageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SageApp
+//
+//  Created by Gabby Banaag on 7/15/20.
+//  Copyright © 2020 Gabbyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
