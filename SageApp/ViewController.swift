f738d410c6e8febcd0988ab0160ca03427e29ad6
gabbybanaag <gb2646@columbia.edu>
2020-07-16 14:16:35 -0700

    separated quotes into arrays

--- SageApp/ViewController.swift
+++ SageApp/ViewController.swift
@@ -11,17 +11,98 @@ import UserNotifications
 
 class ViewController: UIViewController {
     
-    var quotes = [
-        "I am Beyoncé, always.” – Michael Scott",
+    var theOfficeQuotes = [
+        "”I am Beyoncé, always.” – Michael Scott",
         " “‘You miss 100% of the shots you don’t take.’” – Wayne Gretsky’ – Michael Scott",
         " “There’s a lot of beauty in ordinary things. Isn’t that kind of the point?” – Pam Beesly",
         "“I wish there was a way to know you’re in the good old days, before you’ve actually left them.” – Andy Bernard",
-        
-        
+        "“And I knew exactly what to do. But in a much more real sense, I had no idea what to do.” - Michael Scott"
     ]
+    
+    var greysQuotes = [
+          " “Don't let what he wants eclipse what you need. He's very dreamy, but he's not the sun. You are.“ —Cristina Ya season 10, episode 24",
+          "”Walk tall. All you can do is be brave enough to get out there. You fought. You loved. You lost. Walk tall.“ —Mark Sloan, season 5, episode 12",
+          "”Choose me. Love me.“ —Meredith Grey, season 2, episode 5",
+          "”Be unstoppable. Be a force of nature. --- You’re on your own. Be on your own.” —Cristina Yang, season 4, episode 15",
+          "”It's good to be scared. It means you still have something to lose.” —Richard Weber, season 4, episode 10"
+       ]
+    
+    var friendsQuotes = [
+        "”What if I don't want to be a shoe? What if I want to be a purse, you know? Or, a hat?” -Rachel",
+        "”You can’t just give up! Is that what a dinosaur would do?” - Joey",
+        "”How you doin’?” - Joey",
+        "”Sure, I have my bad days, but then I remember what a cute smile I have.” - Chandler",
+        "”It's like a cow’s opinion. It just doesn’t matter. It’s moo.” -Joey"
+    ]
+    
+    var avatarQuotes = [
+        "”In the darkest of times, hope is something you give yourself.” - Zuko",
+        "”Sometimes the best way to solve a problem is to help others.” - Iroh",
+        "”Life happens wherever you are, whether you make it or not.” - Iroh",
+        "“Sometimes life is like this tunnel. You can’t always see the light at the end of the tunnel, but if you keep moving, you will come to a better place.” - Iroh",
+        "“You can’t knock me down!” - Katara",
+        "“”Life happens wherever you are, whether you make it or not.“ -Iroh"
+    ]
+    
+    var criminalQuotes = [
+        "”The best and most beautful things can not be seen or even touched.” - Helen Keller",
+        "”Love is giving someone the ability to destroy you, but trusting them not to”",
+        "”Nothing is so common as the wish to be remarkable.” - Shakespeare",
+        "”In the end, Its not the years in your life that counts. Its the life in your years.” - Abraham Lincoln",
+        "”I remained to inside my head and ended up loosing my mind” - Edgar Allen Poe"
+    ]
+    
+    var goodPlaceQuotes = [
+        "“What matters isn’t if people are good or bad. What matters is, if they’re trying to be better today than they were yesterday.“ -Michael",
+        "”Enlightenment comes from within. The Dalai Llama texted me that.“ – Tahani Al-Jamil",
+        "”Ya basic. And that’s okay.” – Eleanor Shellstrop",
+        "”In case you were wondering, I am, by definition, the best version of myself.” – Janet",
+        "”Pobody’s nerfect.” – Eleanor Shellstrop"
+    ]
+    
+    var strangerQuotes = [
+        "”Friends don't lie.” — Eleven",
+        "”You shouldn't like things because people tell you you're supposed to.” — Jonathan",
+        "”You can't spell America without Erica.” — Erica",
+        "”If we’re both going crazy, then we’ll go crazy together, right?” — Mike",
+        "”If you believe in this story... Finish it.” — Mrs Wheeler"
+    ]
+    
+    var flashQuotes = [
+        "Felicity Smoak: Boom. Drop the mic. It's metaphorical, of course, because we don't have a real mic.",
+        "Cisco Ramon: He's a bad guy; I'm calling it a lair.",
+        "Cisco Ramon: It's a work in progress, Elsa.",
+        "Dr. Harrison Wells: Acceptance is a powerful thing.",
+        "Cisco Ramon: In your dreams, Sister Cold. You know I could call The Flash. He'd be here in like two seconds - probably one."
+    ]
+    
+    var euphoriaQuotes = [
+        "Plenty Of Great, Intelligent, Funny, Interesting, And Creative People Have Struggled With The Same Things You Struggle With.",
+        "That it's about balance, stability. The need to organize her feelings and thoughts, so she can breathe easier.",
+        "“I definitely haven't reached my full power.” - Jules Vaughn",
+        "”I wasn't going to stop until I got the truth.“ - Rue",
+        "“Suddenly, the whole world goes dark and nothing else matters except the person standing in front of you.” - Rue Bennett"
+    ]
+    
+    var umbrellaQuotes = [
+        "“I love you! Even if you can’t love yourself!“ -Klaus",
+        "“Life is short. Future doesn’t come with any guarantees. You want something in life, you gotta go for it.“",
+        "“If you believe in yourself for once, just once, great things are gonna happen for you.“ -Leonard",
+        "The ties that bind you together make you stronger than you are alone. They will make you impervious to the pain and hardship the world will thrust upon you.",
+        "“What’s meant to be is meant to be. That’s our raison d’être.“ -Number 5"
+    ]
+    
+//    var quotes = [
+//        theOfficeQuotes, greysQuotes, friendsQuotes, avatarQuotes,  criminalQuotes, goodPlaceQuotes, strangerQuotes, flashQuotes, euphoriaQuotes, umbrellaQuotes
+//    ]
+    
+    var quotesArray: [[String]] = [[]]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        quotesArray = [theOfficeQuotes, greysQuotes, friendsQuotes, avatarQuotes, criminalQuotes, goodPlaceQuotes, strangerQuotes, flashQuotes, euphoriaQuotes, umbrellaQuotes]
         
         // Ask for permission
         let center = UNUserNotificationCenter.current()
@@ -32,14 +113,14 @@ class ViewController: UIViewController {
         // Create notification content
           let content = UNMutableNotificationContent()
           content.title = "Here's your quote of the day ☀️"
-          content.body = quotes[2]
+          content.body = umbrellaQuotes[1]
         
         // Notif trigger
         let date = Date().addingTimeInterval(15)
         var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
 
-        dateComponents.hour = 11
-        dateComponents.minute = 58
+        dateComponents.hour = 14
+        dateComponents.minute = 14
         
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
